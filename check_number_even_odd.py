132e94c8ef43983449d2abf9ba3f02ad2255fb5a
mangesh <mangeshbabansawant@gmail.com>
2026-03-24 23:14:22 +0530

    check number is odd or even

---
+++ check_number_even_odd.py
@@ -0,0 +1,9 @@
+def is_even(number):
+    if number%2 == 0:
+        return print("Number is prime")
+    else:
+        return print("Number is odd")
+
+number = int(input("Enter the number to check number is prime or not : "))
+
+is_even(number)
